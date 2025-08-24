7a066d5e719cc54a2deb62ded192bbadbdb35465
abhishekv18 <166051197+abhishekv18@users.noreply.github.com>
2025-08-24 13:19:44 +0530

    add

--- src/ScrollToTopButton.jsx
+++ src/ScrollToTopButton.jsx
@@ -17,11 +17,11 @@ const ScrollToTopButton = () => {
     <button
       onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
     
-     className={`fixed block sm:bottom-7 sm:right-1 z-50 group p-2 bg-gradient-to-t from-white via-green-400 to-green-500 hover:bg-green-600 text-white rounded-full shadow-xl 
-        transform transition-all duration-500 ease-out
-        hover:scale-110 hover:-translate-y-1 hover:shadow-2xl
-        active:scale-95 active:transition-none
-        ${show ? 'translate-y-0 opacity-100' : 'translate-y-8 opacity-0 pointer-events-none'}`}
+  className={`fixed hidden sm:block sm:bottom-7 sm:right-1 z-50 group p-2 bg-gradient-to-t from-white via-green-400 to-green-500 hover:bg-green-600 text-white rounded-full shadow-xl 
+  transform transition-all duration-500 ease-out
+  hover:scale-110 hover:-translate-y-1 hover:shadow-2xl
+  active:scale-95 active:transition-none
+  ${show ? 'translate-y-0 opacity-100' : 'translate-y-8 opacity-0 pointer-events-none'}`}
         aria-label="Scroll to top"
         title="Scroll to Top"
        >
